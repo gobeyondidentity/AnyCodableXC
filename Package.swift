c52d63fcf4d33f82783809a1e425c5b2b17618b1
davidb-bi <143821870+davidb-bi@users.noreply.github.com>
2023-09-01 20:58:34 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,6 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AnyCodableXC",
+            path: "Sources/AnyCodable",
             dependencies: []
         ),
         .testTarget(
